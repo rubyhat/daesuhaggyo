6c9270baf83944c60740785ed2c8549eb4c63fc5
rwbykz <dreamervt8@gmail.com>
2021-01-13 09:20:05 +0600

    complete

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,21 @@
-
+'use strict'
 // Scroll with jQuery
-// $(document).ready(function () {
-//   $('.scroll-anch').click(function () {
-//     var scroll_el = $(this).data('target');
-//     if ($(scroll_el).length != 0) {
-//       $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 750);
-//     }
-//     return false;
-//   });
+$(document).ready(function () {
+  $('.scroll-anch').click(function () {
+    var scroll_el = $(this).data('target');
+    if ($(scroll_el).length != 0) {
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 750);
+    }
+    return false;
+  });
 
-//   $('.scroll-anch').click(function () {
-//     $('html body').animate({
-//       scrollTop: $(this).offset().top
-//     }, 750)
-//   });
-// });
+  $('.scroll-anch').click(function () {
+    $('#mobileMenu').modal('hide');
+    $('html body').animate({
+      scrollTop: $(this).offset().top
+    }, 1050)
+  });
+
+  // Animation
+  new WOW().init();
+});
