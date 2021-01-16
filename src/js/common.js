24da2f8562f1abaec5807b5ff36bff0a09445afe
rwbykz <dreamervt8@gmail.com>
2021-01-16 23:33:31 +0600

    form validation updated

--- src/js/common.js
+++ src/js/common.js
@@ -35,4 +35,49 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  // Form validation
+  const firstFormValidation = () => {
+    const form = document.querySelector('.first-form');
+
+    if (form) {
+      const inputs = form.querySelectorAll('.first-form__input'),
+      button = form.querySelector('.first-form__button'),
+      info = form.querySelector('.first-form__success');
+      button.addEventListener('click', () => {
+          if (inputs[0].value !== '' && inputs[1].value !== '') {
+            info.classList.remove('d-none');
+          } else {
+            inputs.forEach(input => {
+              if (input.value === '') {
+                input.classList.add('input-error');
+              }
+            });
+          }
+        });
+    }
+  }
+  firstFormValidation();
+
+  const lastFormValidation = () => {
+    const form = document.querySelector('.feedback__form');
+
+    if (form) {
+      const inputs = form.querySelectorAll('.feedback__input'),
+      button = form.querySelector('.feedback__button'),
+      info = form.querySelector('.feedback__success');
+      button.addEventListener('click', () => {
+          if (inputs[0].value !== '' && inputs[1].value !== '') {
+            info.classList.remove('d-none');
+          } else {
+            inputs.forEach(input => {
+              if (input.value === '') {
+                input.classList.add('input-error');
+              }
+            });
+          }
+        });
+    }
+  }
+  lastFormValidation();
 });
